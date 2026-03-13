9469c01160c2e77c76c47e8defe4cc9e9750ebd9
kazu107 <kazu.uttu107@gmail.com>
2026-03-13 20:56:55 +0900

    markdown

--- components/markdown-block.tsx
+++ components/markdown-block.tsx
@@ -1,10 +1,14 @@
 import ReactMarkdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
 import remarkGfm from "remark-gfm";
+import remarkMath from "remark-math";
 
 export function MarkdownBlock({ text }: { text: string }) {
   return (
     <div className="markdown-block">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{text}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm, remarkMath]} rehypePlugins={[rehypeKatex]}>
+        {text}
+      </ReactMarkdown>
     </div>
   );
 }
